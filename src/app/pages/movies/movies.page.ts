e617b60dc8c850d65140df198af7ac68c1a51334
marcialwushu <marcialwushu@gmail.com>
2019-02-07 17:38:04 -0200

    searchChanged function in moviePage.ts added

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService, SearchType } from '../../services/movie.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  /**
+   * Constructor da nossa primeira página
+   * @param movieService O movie Service para obter dados 
+   */
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  }
+
+  searchChanged() {
+    // Chame a função do serviço que retorna um Observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
   }
 
 }
