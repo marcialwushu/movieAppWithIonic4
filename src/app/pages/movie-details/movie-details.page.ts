b050c116b43fc4158537aa8d8258a2d940663112
marcialwushu <marcialwushu@gmail.com>
2019-02-08 14:20:06 -0200

    ngOnit MovieDetailsPage.ts activatedRoute added

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from './../../services/movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  information = null;
+
+  /**
+   * Constructor da página de detalhes
+   * 
+   * @param activatedRoute Informação sobre a rota utilizada
+   * @param movieService o Serviço movie para obter os dados 
+   */
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    // Obter via GET o ID que foi passado pela url
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    // Obeter via GET as informações da API
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    })
+  }
+
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
   }
 
 }
